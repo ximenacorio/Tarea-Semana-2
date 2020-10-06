1aa338d614bbccd2cadd07f093cbee66f3f9bd72
ximenacorio <71667032+ximenacorio@users.noreply.github.com>
2020-10-06 11:17:26 -0300

    Update ExampleInstrumentedTest.java

--- TareaSemanaDos/login/app/src/androidTest/java/com/ximena/login/ExampleInstrumentedTest.java
+++ TareaSemanaDos/login/app/src/androidTest/java/com/ximena/login/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.juancastano.registrocurso", appContext.getPackageName());
+        assertEquals("com.ximena.login", appContext.getPackageName());
     }
 }
